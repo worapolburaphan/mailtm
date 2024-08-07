390d85f0ef09cb964021b6df8c2625a6cfeba8c8
Worapol_Pan <worapol.pan.buraphan@gmail.com>
2024-08-07 22:08:47 +0700

    feat: new with option

--- mailtm.go
+++ mailtm.go
@@ -7,7 +7,7 @@ import (
 type Service string
 
 const (
-	MAIL_TM Service = "https://api.mail.tm"
+	MailTm Service = "https://api.mail.tm"
 )
 
 type MailClient struct {
@@ -15,6 +15,24 @@ type MailClient struct {
 	service Service
 }
 
+type MailClientOptions struct {
+	Http    *http.Client
+	Service Service
+}
+
+func NewWithOption(options *MailClientOptions) (*MailClient, error) {
+	if options == nil {
+		options = &MailClientOptions{}
+	}
+	if options.Http == nil {
+		options.Http = &http.Client{}
+	}
+	if options.Service == "" {
+		options.Service = MailTm
+	}
+	return &MailClient{http: options.Http, service: options.Service}, nil
+}
+
 func New() (*MailClient, error) {
-	return &MailClient{service: MAIL_TM, http: &http.Client{}}, nil
+	return &MailClient{service: MailTm, http: &http.Client{}}, nil
 }
